ef646aa4650a9d21cf3e3e13ec7541b132d61d21
meizhixing <meizhixing@ynao.ac.cn>
2019-09-21 20:46:10 +0800

    from express_start to static_hupo2018

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 router.get('/',function(req, res, next) {
 //  res.send('Hello router index');
-  res.render('index', {title: 'Virtual Helper is on your service'});
+  res.render('index', {title: '理Note'});
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 var indexRouter = require('./routes/index');
 
 const app = express();
@@ -10,9 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/users',usersRouter);
+app.use('/about',aboutRouter);
 app.use('/',indexRouter);
-// app.get('/',(req,res)=>res.send('Hello World!'));
-
 
 app.listen(5005,()=>console.log('Example app listening on port 5005!'));
